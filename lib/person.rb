26748e322772f28180de4bc947bd4d139469edf6
Amanda Rolon <abrolon87@gmail.com>
2020-01-14 17:38:19 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :height, :eye_color, :weight, :handed, :complexion
+  attr_accessor :name, :birthday, :hair_color, :height, :eye_color, :weight, :handed, :complexion, :t_shirt_size
 
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
